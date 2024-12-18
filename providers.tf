0eee61df1c0bc230c5dcbe67410efd10f26d0890
Peter De Boeck <peter.deboeck@mobilityplus.be>
2024-12-18 07:25:12 +0100

    Remove secret from code

--- providers.tf
+++ providers.tf
@@ -15,8 +15,8 @@ provider "azurerm" {
   skip_provider_registration = "true"
 
   # Connection to Azure
-  subscription_id = "b4600eab-28c7-498e-894a-8df50adbe79a"
-  client_id = "0a7a99e7-bc62-4f40-9d02-4faf94350512"
-  client_secret = "kHO8Q~fW1B-UDc-75Pc7Tn~6zwTQAX.oc0DuMafc"
-  tenant_id = "6488b5da-1e39-400f-8e78-05eb23dfa6f8"
+  subscription_id = var.subscription_id
+  client_id = var.client_id
+  client_secret = var.client_secret
+  tenant_id = var.tenant_id
 }
