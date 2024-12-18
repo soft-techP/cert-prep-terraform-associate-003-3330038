215987b19567dc91c74c059eefc63dc04a554124
Peter De Boeck <peter.deboeck@mobilityplus.be>
2024-12-18 06:48:13 +0100

    Set  Azure provider

--- providers.tf
+++ providers.tf
@@ -1,14 +1,22 @@
+# Configure Azure Provider
 terraform {
   required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.31.0"
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.14.0"
     }
   }
 }
 
-provider "aws" {
-  region = "us-east-1"
-  profile = "Helen"
+provider "azurerm" {
   # Configuration options
+  features {}
+
+  skip_provider_registration = "true"
+
+  # Connection to Azure
+  subscription_id = "b4600eab-28c7-498e-894a-8df50adbe79a"
+  client_id = "0a7a99e7-bc62-4f40-9d02-4faf94350512"
+  client_secret = "kHO8Q~fW1B-UDc-75Pc7Tn~6zwTQAX.oc0DuMafc"
+  tenant_id = "6488b5da-1e39-400f-8e78-05eb23dfa6f8"
 }
